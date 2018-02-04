8f896e98667807a139f7891fce7ce6201ac99d10
ToxcityWare <36136528+ToxcityWare@users.noreply.github.com>
2018-02-04 14:01:18 -0500

    Add files via upload

---
+++ SDK/SDK Headers/EntList.cpp
@@ -0,0 +1,33 @@
+#include "Windows.h"
+#include "EntList.h"
+
+void CEntityList::Function0()
+{
+}
+
+void CEntityList::Function1()
+{
+}
+
+void CEntityList::Function2()
+{
+}
+
+CBaseEntity * CEntityList::GetClientEntity(int entnum)
+{
+	return nullptr;
+}
+
+int CEntityList::GetHighestEntityIndex(void)
+{
+	return 0;
+}
+
+void CEntityList::SetMaxEntities(int maxents)
+{
+}
+
+int CEntityList::GetMaxEntities()
+{
+	return 0;
+}
